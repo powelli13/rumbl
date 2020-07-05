cd23141b41f7391ed4dd3f3e04de088c42e572d5
Isaac Powell <powelli91@outlook.com>
2020-07-05 15:41:49 -0700

    finished ch 4. learned about Ecto changesets and forms in Phoenix.

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,23 +3,32 @@ defmodule Rumbl.Accounts do
   The Accounts context.
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Isaac", username: "isaacpowell"},
-      %User{id: "2", name: "Paul Blart", username: "mallcop"},
-      %User{id: "3", name: "Philip", username: "philipglass"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
   end
 end
