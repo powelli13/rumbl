da71a6335bd6d52d4aefcd30702395fa2c2a17df
Isaac Powell <isaac.powell@sprbrk.com>
2020-07-10 15:06:26 -0700

    more ch 6 progress. learned more about Phoenix actions and Ecto relationships.

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -8,6 +8,10 @@ defmodule Rumbl.Multimedia do
 
   alias Rumbl.Multimedia.Video
 
+  defp user_videos_query(query, %Rumbl.Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+
   @doc """
   Returns the list of videos.
 
@@ -19,6 +23,12 @@ defmodule Rumbl.Multimedia do
   """
   def list_videos do
     Repo.all(Video)
+  end
+
+  def list_user_videos(%Rumbl.Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
   end
 
   @doc """
@@ -37,6 +47,12 @@ defmodule Rumbl.Multimedia do
   """
   def get_video!(id), do: Repo.get!(Video, id)
 
+  def get_user_video!(%Rumbl.Accounts.User{} = user, id) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.get!(id)
+  end
+
   @doc """
   Creates a video.
 
@@ -49,9 +65,10 @@ defmodule Rumbl.Multimedia do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_video(attrs \\ %{}) do
+  def create_video(%Rumbl.Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
   end
 
