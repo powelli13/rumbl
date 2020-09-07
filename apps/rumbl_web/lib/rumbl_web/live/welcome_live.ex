6bac814ea2ab6340ec65391a48c5da208e437bcf
Isaac Powell <powelli91@outlook.com>
2020-09-07 02:18:58 -0700

    setup timer to simulate events

--- apps/rumbl_web/lib/rumbl_web/live/welcome_live.ex
+++ apps/rumbl_web/lib/rumbl_web/live/welcome_live.ex
@@ -7,14 +7,37 @@ defmodule RumblWeb.WelcomeLive do
     ~L"""
     <div>
       <h2><%= @salutation %></h2>
-      <h2>we are <%= @connected %></h2>
+      <h4>Received Items</h4>
+      <%= for item <- @items do %>
+        <div><%= item %></div>
+      <% end %>
     </div>
     """
   end
 
   @impl Phoenix.LiveView
+  def handle_info(:add_item, socket) do
+    item_count = length(socket.assigns[:items])
+
+    {:noreply, update(socket,
+      :items,
+      fn items -> ["new #{item_count}" | items] end)}
+  end
+
+  @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
     salutation = "Welcome to LiveView, from the Programming Phoenix team!"
-    {:ok, assign(socket, salutation: salutation, connected: connected?(socket))}
+
+    if connected?(socket) do
+      # register
+      # :timer.send_interval(3000, self(), :add_item)
+      
+    end
+
+    {:ok, assign(socket,
+      salutation: salutation,
+      connected: connected?(socket),
+      items: [])
+    }
   end
 end
